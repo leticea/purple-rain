1fd4a661a0e393a1c22d3c15f64a490532a641f3
leticea <nascimento.leticeah@gmail.com>
2022-12-26 11:11:36 -0300

    criando funções

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+class Rain {
+  constructor(width, height, direction, posX, posY, speed, color) {
+    this.width = width;
+    this.height = height;
+    this.direction = direction;
+    this.posX = posX;
+    this.posY = posY;
+    this.speed = speed;
+    this.color = color;
+  }
 
+  move() {
+    this.posX += Math.sin(this.direction * Math.PI/2) * this.speed;
+    this.posY += Math.cos(this.direction * Math.PI/2) * this.speed;
+  }
+
+  draw() {
+    canvasContext.rotate(-this.direction);
+    canvasContext.fillStyle = this.color;
+    canvasContext.fillRect(this.posX, this.posY, this.width, this.height);
+    canvasContext.rotate(this.direction);
+  }
+}
 
 let canvas = document.getElementById("canvas");
 
